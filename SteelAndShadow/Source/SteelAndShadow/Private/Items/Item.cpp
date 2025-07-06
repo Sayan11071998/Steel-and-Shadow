107e2ebe2ee7421db5c7bac11a09f1b6e57f014b
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-07 01:11:57 +0530

    Macro Creation

--- SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
@@ -1,4 +1,6 @@
 #include "Items/Item.h"
+#include "DrawDebugHelpers.h"
+#include "SteelAndShadow/SteelAndShadow.h"
 
 AItem::AItem()
 {
@@ -20,9 +22,31 @@ void AItem::BeginPlay()
 			FString("Item OnScreenMessage")
 		);
 	}
+
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+
+	DRAW_SPHERE(Location);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
+
+	if (GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+
+		GEngine->AddOnScreenDebugMessage(
+			1,
+			60.f,
+			FColor::Cyan,
+			Message
+		);
+
+		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
+	}
 }
