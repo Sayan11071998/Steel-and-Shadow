5bcb0f835ed9e0db7754fe56d549bf5a028d2d81
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-06 23:59:43 +0530

    Message Logging on Screen

--- SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
@@ -10,6 +10,16 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play Called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			1,
+			60.f,
+			FColor::Cyan,
+			FString("Item OnScreenMessage")
+		);
+	}
 }
 
 void AItem::Tick(float DeltaTime)
