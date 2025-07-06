1c3b874a557a65d3a1fc7ff9609cf88a9b5a1307
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-07 03:04:58 +0530

    Set Actor Location

--- SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Items/Item.cpp
@@ -11,12 +11,13 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UWorld* World = GetWorld();
+
+	SetActorLocation(FVector(0.f, 0.f, 50.f));
+
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
 
 	DRAW_SPHERE(Location);
-	//DRAW_LINE(Location, Location + Forward * 100.f);
-	//DRAW_POINT(Location + Forward * 100.f);
 	DRAW_VECTOR(Location, Location + Forward * 100.f);
 }
 
