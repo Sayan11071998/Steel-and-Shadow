b177684f38a159dcb93c00e5a773f4cab13c7f6c
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-08 01:07:23 +0530

    Bird Pawn Class Created

---
+++ SteelAndShadow/Intermediate/Build/Win64/UnrealEditor/Inc/SteelAndShadow/UHT/Bird.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Pawns/Bird.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef STEELANDSHADOW_Bird_generated_h
+#error "Bird.generated.h already included, missing '#pragma once' in Bird.h"
+#endif
+#define STEELANDSHADOW_Bird_generated_h
+
+#define FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABird(); \
+	friend struct Z_Construct_UClass_ABird_Statics; \
+public: \
+	DECLARE_CLASS(ABird, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SteelAndShadow"), NO_API) \
+	DECLARE_SERIALIZER(ABird)
+
+
+#define FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ABird(ABird&&); \
+	ABird(const ABird&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABird); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABird); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ABird) \
+	NO_API virtual ~ABird();
+
+
+#define FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_7_PROLOG
+#define FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_10_INCLASS_NO_PURE_DECLS \
+	FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> STEELANDSHADOW_API UClass* StaticClass<class ABird>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_sayan_Projects_Steel_and_Shadow_SteelAndShadow_Source_SteelAndShadow_Public_Pawns_Bird_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ SteelAndShadow/Source/SteelAndShadow/Public/Pawns/Bird.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class STEELANDSHADOW_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+};
