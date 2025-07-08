12e6d70138a7f6b0022274a16e4d5035def2fb80
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-08 13:20:36 +0530

    Move Forward Implemented

--- SteelAndShadow/Source/SteelAndShadow/Private/Pawns/Bird.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Pawns/Bird.cpp
@@ -36,5 +36,9 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ABird::MoveForward(float Value)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), Value);
+	if (Controller && (Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
 }
