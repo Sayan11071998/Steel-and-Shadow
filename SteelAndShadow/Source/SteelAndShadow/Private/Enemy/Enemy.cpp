62af1d7e456c7e64cc9935bf5fab32132ccb2a7b
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-29 00:44:28 +0530

    Direction Hit Reaction

--- SteelAndShadow/Source/SteelAndShadow/Private/Enemy/Enemy.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Enemy/Enemy.cpp
@@ -43,7 +43,6 @@ void AEnemy::PlayHitReactMontage(const FName& SectionName)
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
 	DRAW_SPHERE_COLOR(ImpactPoint, FColor::Orange);
-	PlayHitReactMontage(FName("FromLeft"));
 
 	const FVector Forward = GetActorForwardVector();
 	const FVector ImpactLowered(ImpactPoint.X, ImpactPoint.Y, GetActorLocation().Z);
@@ -59,6 +58,23 @@ void AEnemy::GetHit(const FVector& ImpactPoint)
 	{
 		Theta *= -1.f;
 	}
+
+	FName Section("FromBack");
+
+	if (Theta >= -45.f && Theta < 45.f)
+	{
+		Section = FName("FromFront");
+	}
+	else if (Theta >= -135.f && Theta < -45.f)
+	{
+		Section = FName("FromLeft");
+	}
+	else if (Theta >= 45.f && Theta < 135.f)
+	{
+		Section = FName("FromRight");
+	}
+
+	PlayHitReactMontage(Section);
 
 	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + CrossProduct * 100.f, 5.f, FColor::Red, 5.f);
 
