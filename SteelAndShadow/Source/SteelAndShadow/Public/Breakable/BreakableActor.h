acc285ded08f2c90b05e3aaa0a9ce19c90d05115
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-08-03 02:26:02 +0530

    Spawning Actors

--- SteelAndShadow/Source/SteelAndShadow/Public/Breakable/BreakableActor.h
+++ SteelAndShadow/Source/SteelAndShadow/Public/Breakable/BreakableActor.h
@@ -19,7 +19,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TSubclassOf<class ATreasure> TreasureClass;
 };
