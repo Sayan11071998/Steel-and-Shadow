8c2e1fc297f4cfac5dee7485dbb9782ee942daae
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-09-09 02:01:23 +0530

    Raptor Attack Montage

--- SteelAndShadow/Source/SteelAndShadow/Private/Enemy/Enemy.cpp
+++ SteelAndShadow/Source/SteelAndShadow/Private/Enemy/Enemy.cpp
@@ -295,7 +295,7 @@ void AEnemy::MoveToTarget(AActor* Target)
 
 	FAIMoveRequest MoveRequest;
 	MoveRequest.SetGoalActor(Target);
-	MoveRequest.SetAcceptanceRadius(50.f);
+	MoveRequest.SetAcceptanceRadius(AcceptanceRadius);
 	EnemyController->MoveTo(MoveRequest);
 }
 
